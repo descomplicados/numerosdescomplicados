7ad926d3ceff97936c62dc81573775bc4d27a9e8
descomplicados <ndescomplicados@gmail.com>
2025-09-23 13:53:28 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+const menu = document.getElementById('mobile-menu');
+const navList = document.querySelector('.nav-list');
+
+menu.addEventListener('click', () => {
+  navList.classList.toggle('active');
+});
+</script>
